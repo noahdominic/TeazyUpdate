83febc3abe54f5aca28fba0e3aeaccc80b90deed
Noah Dominic Miranda Silvio <ndsilvio@outlook.ph>
2017-04-09 00:43:12 +0800

    Update AllTasks.java

--- AllTasks.java
+++ AllTasks.java
@@ -1,21 +1,66 @@
 import org.lwjgl.input.Mouse;
+
+import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.Vector;
+
 import org.newdawn.slick.*;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.font.effects.ColorEffect;
+import org.newdawn.slick.geom.*;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.util.ResourceLoader;
 
-/*
-*
+/**
  * Created by Marie Curie on 01/04/2017.
-*/
-
+ */
 public class AllTasks extends BasicGameState {
+
+    //Logic Shit
+    private static final int ALL_TASKS = 0;
+    private static final int TODAY_VIEW = 1;
+    private static final int WEEKLY_VIEW = 2;
+    private static final int SELECTOR_HEIGHT = 34;
+    private static final int LEFTPANEL_WIDTH = 225;
+
+    private boolean isAddingNewTask = false;
+    private boolean hasSelectedNewTask = false;
+    private boolean hasSelectedDeadline = false;
+    private boolean hasSelectedCategory = false;
     public String mouse = "";
-    public boolean addtask = false;
-    public TextField taskname;
-    public TextField duedate;
-    public TextField setcategory;
-    public boolean isFirstTimeTaskNameAT = true , isFirstTimeDueDateAT = true , isFirstTimeSetCategoryAT = true;
+    private int currentView = ALL_TASKS;
+    private int topYPos;
+
+
+    //UI Shit
+    Font sanFranUITxtRegJava = null;
+    Font sanFranUITxtBoldJava = null;
+    UnicodeFont sanFranTxReg = null;
+    UnicodeFont sanFranTxRegGrey = null;
+    UnicodeFont sanFranTxBold = null;
+    Image bg = null;
+    Image leftpanel = null;
+    Image iconalltasks = null;
+    Image icontoday = null;
+    Image iconthisweek = null;
+    Image iconcategory = null;
+    Image selectorview = null;
+    Image empty = null;
+    Image addnewcategory = null;
+    Image fab = null;
+    Image newTaskWindow = null;
+    Image taskBox = null;
+    Image boxNotDone = null;
+    Image textbox = null;
+    Vector<Image> taskBoxVector = new Vector();
+    Vector<Task> taskVector = new Vector();
+    String[] views = new String[]{"All Tasks", "Today", "This Week"};
+    TextField taskName, deadline, category;
 
     public AllTasks(int alltasks) {
     }
@@ -27,79 +72,139 @@ public class AllTasks extends BasicGameState {
 
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
-        isFirstTimeTaskNameAT = isFirstTimeDueDateAT = isFirstTimeSetCategoryAT = true;
-        addtask = false;
+        topYPos = 0;
+        //Loading
+        try {
+            bg = new Image("res/Components/04 home/bg-main.png");
+            leftpanel = new Image("res/Components/04 home/leftpanel.png");
+            iconalltasks = new Image("res/Components/04 home/iconalltasks.png");
+            icontoday = new Image("res/Components/04 home/icontoday.png");
+            iconthisweek = new Image("res/Components/04 home/iconthisweek.png");
+            iconcategory = new Image("res/Components/04 home/iconcategory.png");
+            selectorview = new Image("res/Components/04 home/selectorview.png");
+            empty = new Image("res/Components/04 home/empty.png");
+            addnewcategory = new Image("res/Components/04 home/addnewcategory.png");
+            fab = new Image("res/Components/04 home/fab.png");
+            newTaskWindow = new Image("res/Components/04 home/newtaskwindow.png");
+            taskBox = new Image("res/Components/04 home/taskbox.png");
+            boxNotDone = new Image("res/Components/04 home/boxnotdone.png");
+            sanFranUITxtRegJava = Font.createFont(Font.TRUETYPE_FONT, ResourceLoader.getResourceAsStream("res/Fonts/SFUIText-Regular.ttf"));
+            sanFranUITxtBoldJava = Font.createFont(Font.TRUETYPE_FONT, ResourceLoader.getResourceAsStream("res/Fonts/SFUIText-Bold.ttf"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        sanFranUITxtRegJava = sanFranUITxtRegJava.deriveFont(Font.PLAIN, 12.f);
+        sanFranUITxtBoldJava = sanFranUITxtBoldJava.deriveFont(Font.PLAIN, 12.f);
+
+        for(int i = 0; i < 5 ; i++) {
+            taskVector.addElement(new Task("This is Task No" + i));
+        }
+
+        //Since fonts are a kind of a bitch, they'll need extra steps before rendering
+        sanFranTxReg = new UnicodeFont(sanFranUITxtRegJava);
+        sanFranTxReg.addAsciiGlyphs();
+        sanFranTxReg.getEffects().add(new ColorEffect(Color.black));
+        try {
+            sanFranTxReg.loadGlyphs();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+
+        sanFranTxBold = new UnicodeFont(sanFranUITxtBoldJava);
+        sanFranTxBold.addAsciiGlyphs();
+        sanFranTxBold.getEffects().add(new ColorEffect(Color.black));
+        try {
+            sanFranTxBold.loadGlyphs();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+
+        sanFranTxRegGrey = new UnicodeFont(sanFranUITxtRegJava);
+        sanFranTxRegGrey.addAsciiGlyphs();
+        sanFranTxRegGrey.getEffects().add(new ColorEffect(Color.gray));
+        try {
+            sanFranTxRegGrey.loadGlyphs();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+
+        taskName = new TextField(container, sanFranTxReg, 502, 540-161, 224, 20);
+        deadline = new TextField(container, sanFranTxReg, 502, 540-125, 224, 20);
+        category = new TextField(container, sanFranTxReg, 502, 540-89, 244, 20);
+
+        initialize(taskName);
+        initialize(deadline);
+        initialize(category);
+
         container.setShowFPS(false);
-
-        taskname = new TextField(container, container.getDefaultFont(), 495, 375, 270, 27);
-        duedate = new TextField(container, container.getDefaultFont(), 495, 409, 270, 27);
-        setcategory = new TextField(container, container.getDefaultFont(), 495, 444, 270, 27);
-
-        taskname.setBorderColor(Color.white);
-        taskname.setBackgroundColor(Color.white);
-        taskname.setTextColor(Color.gray);
-        taskname.setCursorVisible(false);
-        taskname.setFocus(false);
-        taskname.setConsumeEvents(false);
-        taskname.setAcceptingInput(true);
-        taskname.setText("Write task name");
-
-        duedate.setBorderColor(Color.white);
-        duedate.setBackgroundColor(Color.white);
-        duedate.setTextColor(Color.gray);
-        duedate.setCursorVisible(false);
-        duedate.setFocus(false);
-        duedate.setConsumeEvents(false);
-        duedate.setAcceptingInput(true);
-        duedate.setText("Set due date");
-
-        setcategory.setBorderColor(Color.white);
-        setcategory.setBackgroundColor(Color.white);
-        setcategory.setTextColor(Color.gray);
-        setcategory.setCursorVisible(false);
-        setcategory.setFocus(false);
-        setcategory.setConsumeEvents(false);
-        setcategory.setAcceptingInput(true);
-        setcategory.setText("Set category");
-
-
     }
 
     @Override
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-        Image bg = new Image("res/Components/04 home/bg-main.png");
-        Image leftpanel = new Image("res/Components/04 home/leftpanel.png");
-        Image iconalltasks = new Image("res/Components/04 home/iconalltasks.png");
-        Image icontoday = new Image("res/Components/04 home/icontoday.png");
-        Image iconthisweek = new Image("res/Components/04 home/iconthisweek.png");
-        Image iconcategory = new Image("res/Components/04 home/iconcategory.png");
-        Image selectorview = new Image("res/Components/04 home/selectorview.png");
-        Image empty = new Image("res/Components/04 home/empty.png");
-        Image addnewcategory = new Image("res/Components/04 home/addnewcategory.png");
-        Image fab = new Image("res/Components/04 home/fab.png");
-        Image taskbox = new Image("res/Components/04 home/taskbox.png");
-        Image newtaskwindow = new Image("res/Components/04 home/newtaskwindow.png");
+        renderLeftBar(g, currentView);
+        renderAllTasks(g);
+        if(isAddingNewTask){
+            renderNewTask(container, g);
+        } else {
+            renderFAB(g);
+        }
+    }
 
+    private void renderLeftBar(Graphics g, int currentView) {
+        //Rendering
         g.drawImage(bg,0,0);
         g.drawImage(leftpanel, 0,0);
-        g.drawImage(selectorview,0,145);
+        g.drawImage(selectorview,0, 145 + selectorview.getHeight()*currentView);
         g.drawImage(iconalltasks,25, 155);
         g.drawImage(icontoday,25, 145+34+(34-24)/2);
         g.drawImage(iconthisweek,25,145+34*2+(34-24)/2);
         g.drawImage(iconcategory,25,280);
-        g.drawImage(empty, 320, 110);
-        if (!addtask) {
-            g.drawImage(fab, 730, 460);
-        }
-        else{
-            g.drawImage(newtaskwindow,460,325);
-            taskname.render(container,g);
-            duedate.render(container,g);
-            setcategory.render(container,g);
-        }
-
         g.drawImage(addnewcategory, 30,320);
         g.drawString(mouse, 50, 100);
+
+        if(taskVector.isEmpty()){
+            g.drawImage(empty, 320, 110);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if(i == currentView) {
+                sanFranTxBold.drawString(75, 155 + selectorview.getHeight()*i, views[i]);
+            } else {
+                sanFranTxReg.drawString(75, 155 + selectorview.getHeight()*i, views[i]);
+            }
+        }
+    }
+
+    private void renderNewTask(GameContainer container, Graphics g) {
+
+        g.drawImage(newTaskWindow, 460, 325);
+
+        if(!hasSelectedNewTask && taskName.getText().equals("")) {
+            sanFranTxRegGrey.drawString(502, 540-161, "Type Task Name");
+        }
+        if(!hasSelectedDeadline && deadline.getText().equals("")) {
+            sanFranTxRegGrey.drawString(502, 540-125, "Set due date (MM/DD/YY)");
+        }
+        if(!hasSelectedCategory && category.getText().equals("")) {
+            sanFranTxRegGrey.drawString(502, 540-89, "Set category");
+        }
+
+        taskName.render(container, g);
+        deadline.render(container, g);
+        category.render(container, g);
+    }
+
+    private void renderAllTasks(Graphics g) {
+        for(int i = 0; i < taskVector.size(); i++) {
+            g.drawImage(taskBox, 90*3, topYPos + 10 + (40 + 10) * i);
+            sanFranTxReg.drawString(90*3 + 40, topYPos + 10 + 11 + (40 + 10) * i, taskVector.elementAt(i).getDescription());
+            g.drawImage(boxNotDone, 90*3 + 12, topYPos + 10 + 11 + (40 + 10) * i) ;
+        }
+    }
+
+    private void renderFAB(Graphics g) {
+        g.drawImage(fab,730,460);
     }
 
     @Override
@@ -110,72 +215,95 @@ public class AllTasks extends BasicGameState {
 
         Input input = container.getInput();	//keyboard and mouse input
 
-        if((xpos>735 && xpos<781) && (ypos>28 && ypos<77) ){
-            if (!addtask) {
-                if(input.isMousePressed(0)){
-                    addtask = true; //mugawas ang new task window
+        if(hasSelectedNewTask){
+            taskName.setFocus(true);
+        }
+
+        if(hasSelectedDeadline){
+            deadline.setFocus(true);
+        }
+
+        if(hasSelectedCategory){
+            category.setFocus(true);
+        }
+
+        if(input.isMousePressed(0)) {
+            if(isAddingNewTask) {
+                if((xpos > 471 && xpos < 782) && (ypos > 29 && ypos < 207) ){
+                    //if mouse is in newTaskBox area
+                    if(xpos > 496 && xpos < 766) {
+                        //is mouse is in xpos range of the textboxes in taskBox
+                        if(ypos > 137 && ypos < 162) {
+                            //has selected new task
+                            hasSelectedNewTask = true;
+                            hasSelectedDeadline = false;
+                            hasSelectedCategory = false;
+                        }else if(ypos > 106 && ypos < 129) {
+                            //has selected deadline
+                            hasSelectedNewTask = false;
+                            hasSelectedDeadline = true;
+                            hasSelectedCategory = false;
+                        }else if(ypos > 69 && ypos < 93) {
+                            //has selected category
+                            hasSelectedNewTask = false;
+                            hasSelectedDeadline = false;
+                            hasSelectedCategory = true;
+                        } else if((xpos>730 && xpos<766) && (ypos>46 && ypos<60) ) {
+                            //done
+                            hasSelectedNewTask = false;
+                            hasSelectedDeadline = false;
+                            hasSelectedCategory = false;
+                            isAddingNewTask = false;
+                            taskVector.addElement(new Task (taskName.getText()));
+                            initialize(taskName);
+                        } else {
+                            //didn't click on any interactable object on newTaskBox
+                            hasSelectedNewTask = false;
+                            hasSelectedDeadline = false;
+                            hasSelectedCategory = false;
+                        }
+                    }
+                } else {
+                    //if newTaskBox is open but user clicked outside the newTaskBox, the TaskBox closes
+                    isAddingNewTask = false;
+                    initialize(taskName);
+                }
+            }
+            else {
+                if (xpos >= 0 && xpos <= LEFTPANEL_WIDTH) {
+                    if (ypos <= (540 - 145) - SELECTOR_HEIGHT && ypos >= (540 - 145) - SELECTOR_HEIGHT) {
+                        currentView = ALL_TASKS;
+                    }
+
+                    if (ypos <= (540 - 145) - SELECTOR_HEIGHT * TODAY_VIEW && ypos >= (540 - 145) - SELECTOR_HEIGHT * (TODAY_VIEW + 1)) {
+                        currentView = TODAY_VIEW;
+                    }
+
+                    if (ypos <= (540 - 145) - SELECTOR_HEIGHT * WEEKLY_VIEW && ypos >= (540 - 145) - SELECTOR_HEIGHT * (WEEKLY_VIEW + 1)) {
+                        currentView = WEEKLY_VIEW;
+                    }
+                }
+
+                if((xpos>735 && xpos<781) && (ypos>28 && ypos<77) ) {//forFAB
+                    isAddingNewTask = true; //mugawas ang new task window
                 }
             }
         }
 
-        if((xpos>730 && xpos<766) && (ypos>46 && ypos<60) ){
-            if (addtask) {
-                if(input.isMousePressed(0)){
-                    addtask = false; //mugawas ang new task window
-                }
-            }
+        //if (bottomYPos > 510)
+        if(!isAddingNewTask) {
+            topYPos = topYPos + Mouse.getDWheel()/8;
         }
+    }
 
-        if((xpos>0 && xpos<225) && (ypos>328 && ypos<356) ){
-            if(input.isMouseButtonDown(0)){
-                isFirstTimeTaskNameAT = isFirstTimeDueDateAT = isFirstTimeSetCategoryAT = true;
-                game.enterState(5); //go to today view
-            }
-        }
-        if((xpos>0 && xpos<225) && (ypos>294 && ypos<325) ){
-            if(input.isMouseButtonDown(0)){
-                isFirstTimeTaskNameAT = isFirstTimeDueDateAT = isFirstTimeSetCategoryAT = true;
-                game.enterState(6); //go to week view
-            }
-        }
-        if((xpos>0 && xpos<225) && (ypos>234 && ypos<261) ){
-            if(input.isMouseButtonDown(0)){
-                game.enterState(7); //go to category state
-            }
-        }
-
-        if(isFirstTimeTaskNameAT && taskname.hasFocus() && addtask) {
-            taskname.setText("");
-            taskname.setTextColor(Color.black);
-            isFirstTimeTaskNameAT = false;
-        }
-
-        if(isFirstTimeDueDateAT && duedate.hasFocus() && addtask) {
-            duedate.setText("");
-            duedate.setTextColor(Color.black);
-            isFirstTimeDueDateAT = false;
-        }
-        if(isFirstTimeSetCategoryAT && setcategory.hasFocus() && addtask) {
-            setcategory.setText("");
-            setcategory.setTextColor(Color.black);
-            isFirstTimeSetCategoryAT = false;
-        }
-        if (!taskname.hasFocus() && taskname.getText().equals("")){
-            taskname.setTextColor(Color.gray);
-            taskname.setText("Write task name");
-            isFirstTimeTaskNameAT = true;
-        }
-        if (!duedate.hasFocus() && duedate.getText().equals("")){
-            duedate.setTextColor(Color.gray);
-            duedate.setText("Set due date");
-            isFirstTimeDueDateAT = true;
-        }
-        if (!setcategory.hasFocus() && setcategory.getText().equals("")){
-            setcategory.setTextColor(Color.gray);
-            setcategory.setText("Current school");
-            isFirstTimeSetCategoryAT = true;
-        }
-
+    private static void initialize (TextField taskname){
+        taskname.setBorderColor(org.newdawn.slick.Color.transparent);
+        taskname.setBackgroundColor(org.newdawn.slick.Color.transparent);
+        taskname.setTextColor(org.newdawn.slick.Color.black);
+        taskname.setCursorVisible(false);
+        taskname.setFocus(false);
+        taskname.setConsumeEvents(true);
+        taskname.setAcceptingInput(true);
+        taskname.setText("");
     }
 }
-
